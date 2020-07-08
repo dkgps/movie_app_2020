e9412950ab53140cae8e0b42e2f6c8410db08a9a
Dkgps <dkgps@icloud.com>
2020-07-08 16:30:30 +0900

    React App Upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+ReactDOM.render(
+  
+    <App />
+  ,
+  document.getElementById('root')
+); //아이디가 'root'인 엘리먼트에 App 컴포넌트를 그린다
+
+

d4ee974b533f09e69d348c63d202b7cfbd43fe37
Dkgps <dkgps@icloud.com>
2020-07-15 16:11:32 +0900

    react Example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
+function Food({name, picture}){
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id : 1,
+    name: 'Tteokbokki',
+    image: 'http://blogfiles.naver.net/data20/2006/8/21/296/2723_1-withy76.gif',
+    rating : 5,
+  },
+  {
+    id : 2,
+    name: 'Chicken',
+    image: 'http://imgnews.naver.net/image/016/2017/03/02/20170302000582_0_20170302112306658.jpg',
+    rating : 4.9,
+  },
+  {
+    id: 3,
+    name: 'Kimchibokkeumbap',
+    image: 'http://blogfiles.naver.net/20160320_16/redzacal_1458464095457BuyXQ_JPEG/%B1%E8%C4%A1%BA%BA%C0%BD%B9%E4%2C%B0%D4%BB%EC%BD%BA%C7%C1_010.jpg',
+    rating : 4.5,
+  }
+];
+
 function App() {
-  return <div>Hello!!!!!!!</div>;
+  return (
+  <div>
+    {foodILike.map(dish => (<Food key ={dish.id} name={dish.name} picture={dish.image}/>))}
+    
+  </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-ReactDOM.render(
-  
-    <App />
-  ,
-  document.getElementById('root')
-); //아이디가 'root'인 엘리먼트에 App 컴포넌트를 그린다
+ReactDOM.render(<App />,document.getElementById('root')); //아이디가 'root'인 엘리먼트에 App 컴포넌트를 그린다
 
 

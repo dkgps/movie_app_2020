84b9731960a1238189c26c9c165ebb413c6fcde3
Dkgps <dkgps@icloud.com>
2020-07-15 16:32:32 +0900

    react Example complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({name, picture}){
+function Food({name, picture, rating}){
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
   );
@@ -33,10 +35,17 @@ const foodILike = [
 function App() {
   return (
   <div>
-    {foodILike.map(dish => (<Food key ={dish.id} name={dish.name} picture={dish.image}/>))}
+    {foodILike.map(dish => (<Food key ={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>))}
     
   </div>
   );
 }
+
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 export default App;
